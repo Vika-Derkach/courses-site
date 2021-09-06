527f1e89844a750121f7590ca3874f334a466c37
Vika <queenviktoriyphp@gmail.com>
2021-09-06 17:49:05 +0300

    create course

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,17 +1,18 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-  return {
-    type: types.CREATE_COURSE,
-    course,
-  };
-}
 export function loadCourseSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
     courses,
   };
+}
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 export function loadCourses() {
   return function (dipatch) {
@@ -19,6 +20,20 @@ export function loadCourses() {
       .getCourses()
       .then((courses) => {
         dipatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+export function saveCourse(course) {
+  return function (dispatch) {
+    return courseApi
+      .saveCourse(course)
+      .then((savedCourse, getState) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch((error) => {
         throw error;
